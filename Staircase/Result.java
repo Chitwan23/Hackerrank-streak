8dd7dc5a4b105a50005a3c2c1b35e17380c37da4
Chitwan Singh <110717037+Chitwan23@users.noreply.github.com>
2023-10-07 03:52:21 +0530

    Result.java

---
+++ Staircase/Result.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class Result {
+    public static void staircase(int n) {
+        for (int i = 1; i <= n; i++) {
+            int spaces = n - i;
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
